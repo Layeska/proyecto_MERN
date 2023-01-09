a667713debb03ecb1d9945303cbc8f850a92b2c8
Ana Juárez <lisa13suarez@gmail.com>
2023-01-08 23:46:38 -0600

    editando variables

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-    host: process.env.DB_PORT || "localhost", //127.0.0.1
+    host: process.env.DB_HOST || "localhost", //127.0.0.1
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER ||"root",
     password: process.env.DB_PASSWORD ||"ana1234",
