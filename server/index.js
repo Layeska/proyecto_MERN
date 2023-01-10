ecafd40a57224d39f9491e15da106b2be3bbd7f6
Ana Juárez <lisa13suarez@gmail.com>
2023-01-09 21:56:12 -0600

    editando codigo

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import {PORT} from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
 
-import { expressCspHeader, INLINE, NONE, SELF, NONCE } from 'express-csp-header';
 
 const app = express();
 const _dirName = dirname(fileURLToPath(import.meta.url));
@@ -15,17 +14,6 @@ const _dirName = dirname(fileURLToPath(import.meta.url));
 app.use(cors());
 app.use(express.json());
 
-
-app.use(expressCspHeader({
-    directives: {
-        'default-src': [NONE],
-        'script-src': [NONE],
-        'style-src': [SELF, 'https://cdn.tailwindcss.com'],
-        'font-src': [SELF, 'https://fonts.googleapis.com'],
-        'worker-src': [NONE],
-        'block-all-mixed-content': true
-    }
-}));
 
 app.use(indexRoutes);
 app.use(tasksRoutes);
