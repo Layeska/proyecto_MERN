4e736dc79cdb8ab9d391fb5f6df4678795c6a8de
Ana Juárez <lisa13suarez@gmail.com>
2023-01-09 22:31:58 -0600

    agregando variable global

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_URL;
 
-export const getTasksRequest = async () => await axios.get('http://localhost:8000/tasks');
+export const getTasksRequest = async () => await axios.get(`${API_URL}/tasks}`);
 
-export const createTasksRequest = async (tasks) => await axios.post('http://localhost:8000/tasks', tasks);
+export const createTasksRequest = async (tasks) => await axios.post(`${API_URL}/tasks`, tasks);
 
-export const deleteTasksRequest = async (id) => await axios.delete(`http://localhost:8000/tasks/${id}`);
+export const deleteTasksRequest = async (id) => await axios.delete(`${API_URL}/tasks/${id}`);
 
-export const getTaskRequest = async (id) => await axios.get(`http://localhost:8000/tasks/${id}`);
+export const getTaskRequest = async (id) => await axios.get(`${API_URL}/tasks/${id}`);
 
-export const updateTaskRequest = async (id, newTask) => await axios.put(`http://localhost:8000/tasks/${id}`, newTask);
+export const updateTaskRequest = async (id, newTask) => await axios.put(`${API_URL}/tasks/${id}`, newTask);
 
-export const toggleTaskRequest = async (id, done) => await axios.put(`http://localhost:8000/tasks/${id}`, {done});
+export const toggleTaskRequest = async (id, done) => await axios.put(`${API_URL}/tasks/${id}`, {done});
