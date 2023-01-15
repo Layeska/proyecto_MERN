5cf9dfac2ef78547b49a2154f1582f9cfac5dc99
Ana Juárez <lisa13suarez@gmail.com>
2023-01-15 02:47:58 -0600

    editando tailwind

--- client/src/pages/TasksForm.jsx
+++ client/src/pages/TasksForm.jsx
@@ -26,8 +26,8 @@ const TasksForm = () => {
 
         loadTasks();
     }, []);
-    const alert = useAlert()
-    
+
+    const alert = useAlert();
 
     return (
         <div className=''>
@@ -48,8 +48,28 @@ const TasksForm = () => {
                         alert.show('Por favor ingresar tarea!')
                     }
             }}>
-                {({handleChange, handleSubmit, values, isSubmitting}) => (
-                    <Form onSubmit={handleSubmit} className='bg-amber-500 max-w-lg max-h-full rounded-md p-4 mx-auto mt-10'>
+            {({handleChange, handleSubmit, values, isSubmitting}) => (
+                <Form onSubmit={handleSubmit} className='bg-white max-w-lg max-h-full rounded-md p-4 mx-auto flex-col justify-center'>
+                    <h2 className='text-xl font-bold uppercase text-center mb-5'>{params.id ? 'Editar' : 'Crear'} Tareas</h2>
+                    <label htmlFor="#" className='block'>Ingrese tarea</label>
+                    <input onChange={handleChange} value={values.title} className='bg-orange-100 px-2 py-1 rounded-sm w-full placeholder-gray-700' type="text" name='title' placeholder='Homework'/>
+                    
+                    <label htmlFor="#" className='block mt-4'>Description</label>
+                    <input onChange={handleChange} value={values.description} className='bg-orange-100 px-2 py-1 rounded-sm w-full placeholder-gray-700' type="text" name='description' placeholder='Description'/>
+                    <button type='submit' disabled={isSubmitting} className='block bg-amber-900 px-2 py-1 text-white w-full rounded-md mt-3'>
+                    {isSubmitting ? 'Guardando...' : 'Guardar'}
+                    </button>
+                </Form>
+            )}
+            </Formik>
+        </div>
+    )
+}
+
+export default TasksForm
+
+/*
+<Form onSubmit={handleSubmit} className='bg-amber-500 max-w-lg max-h-full rounded-md p-4 mx-auto mt-10'>
                         <h2 className='text-xl font-bold uppercase text-center'>{params.id ? 'Editar' : 'Crear'} Tareas</h2>
                         <label className='block'>Tarea</label>
                         <input type='text' name='title' placeholder='Ingresa tarea' onChange={handleChange} value={values.title} className='px-2 py-1 rounded-sm w-full placeholder-gray-700'/>
@@ -58,11 +78,5 @@ const TasksForm = () => {
                         <textarea name='description' rows='3' placeholder='Escribe detalles...' onChange={handleChange} value={values.description} className='px-2 py-1 rounded-sm w-full placeholder-gray-700'></textarea>
                         <button type='submit' disabled={isSubmitting} className='block bg-amber-800 px-2 py-1 text-white w-full rounded-md'> {isSubmitting ? 'Guardando...' : 'Guardar'} </button>
                     </Form>
-                )}
-            </Formik>
-            
-        </div>
-    )
-}
 
-export default TasksForm
+*/
