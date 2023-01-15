c8f8974288c993f3666793992ceb3fe2895fe35a
Ana Juárez <lisa13suarez@gmail.com>
2023-01-15 02:39:03 -0600

    editando tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
+import {TasksContextProvider} from './context/Tasks.context';
 import Tasks from './pages/Tasks';
 import TasksForm from './pages/TasksForm';
 import NotFound from './pages/NotFount';
 import NavBar from './components/NavBar';
-import {TasksContextProvider} from './context/Tasks.context';
 
 function App() {
   return (
-    <div className=''>
+    <>
       <NavBar/>
-      <div className='container mx-auto py-4 px-20'>
-        <TasksContextProvider>
-          <Routes>
-            <Route path='/' element={<Tasks/>}/>
-            <Route path='/new' element={<TasksForm/>}/>
-            <Route path='/edit/:id' element={<TasksForm/>}/>
-            <Route path='*' element={<NotFound/>}/>
-          </Routes>
-        </TasksContextProvider>
+      <main className='bg-orange-300 flex justify-center items-start h-screen font-mono'>
+        <div className='w-full max-w-3xl mx-3 mt-10'>
+          <>
+            <h1 className='text-center mb-5 text-5xl uppercase font-bold italic'>Todo List</h1>
+              <TasksContextProvider>
+                  <Routes>
+                    <Route path='/' element={<Tasks/>}/>
+                    <Route path='/new' element={<TasksForm/>}/>
+                    <Route path='/edit/:id' element={<TasksForm/>}/>
+                    <Route path='*' element={<NotFound/>}/>
+                  </Routes>
+              </TasksContextProvider>
+          </>
       </div>
-    </div>
-  )
+    </main>
+  </>
+  );
 }
 
-export default App
+export default App;
