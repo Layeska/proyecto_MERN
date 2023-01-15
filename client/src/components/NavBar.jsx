72258f018862006abb64c38da9300b17bea37fc4
Ana Juárez <lisa13suarez@gmail.com>
2023-01-15 02:40:39 -0600

    editando tailwind

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { BiListUl } from 'react-icons/bi';
 
 const NavBar = () => {
     return (
         <div className='bg-neutral-800 flex justify-between px-20 py-4'>
-            <Link to='/' className='text-white font-bold'> <h1>React - MySQL - Tailwind - Express - Node</h1> </Link>
+            <Link to='/' className='text-white font-bold flex gap-3 justify-between items-center content-center'> 
+                <BiListUl/>
+                <h2 className='text-lg'>App</h2>
+            </Link>
             <ul className='flex gap-x-1'>
                 <li>
-                    <Link to={"/"} className='bg-slate-200 px-2 py-1 text-sm'>Home</Link>
+                    <Link to={'/'} className='bg-orange-100 px-2 py-1 text-sm rounded-xl'>Home</Link>
                 </li>
                 <li>
-                    <Link to={"/new"} className='bg-teal-200 px-2 py-1 text-sm'>Agregar</Link>
+                    <Link to={'/new'} className='bg-orange-400 px-2 py-1 text-sm rounded-xl'>Agregar</Link>
                 </li>
             </ul>
         </div>
-    )
+    );
 }
 
 export default NavBar;
