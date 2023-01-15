3322029fe7fb4bfcc5ba9a94de494786018be0b9
Ana Juárez <lisa13suarez@gmail.com>
2023-01-15 02:47:40 -0600

    editando tailwind

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect }from 'react';
 import TasksItem from '../components/TasksItem';
 import { useTasks } from '../context/Tasks.context';
 
 const Tasks = () => {
     const {tasks, loadTasks} = useTasks();
+    let fecha = new Date().toDateString();
 
     useEffect(() => {
         loadTasks();
@@ -12,28 +12,41 @@ const Tasks = () => {
 
     function renderMain() {
         if(tasks.length == 0){
-            return <h1 className=' font-bold'>No hay notas aun!</h1>
+            return (
+                <tr className='odd:bg-orange-100 even:bg-orange-50 text-orange-800'>
+                    <td className='p-2 text-center'></td>
+                    <td className='p-2 text-left text-gray-700'>No hay tareas!</td>
+                    <td></td>
+                </tr>
+            );
         }
-        return tasks.map((task) => <TasksItem task={task} key={task.id}/>);
+
+        return tasks.map((task) => <TasksItem task={task} key={task.id}/> );
     }
 
+
     return (
-        <div>
-            <h1 className='pb-5 text-5xl font-bold text-center'>Lista de tareas</h1>
-            <div className='flex flex-col gap-3 justify-center items-center'>
-                {renderMain()}
+        <div className='bg-white mt-5 p-4 rounded-lg shadow-lg'>
+            <div>
+                <h2 className='text-lg font-bold mb-0 leading-none text-gray-800'>Todo's</h2>
+                <small className='text-xs italic inline-block mt-0 text-gray-700'>{`${fecha}`}</small>
+            </div>
+            <div className='mt-3 overflow-y-auto max-h-80'>
+                <table className='w-full'>
+                    <thead>
+                        <tr className='bg-orange-500 text-orange-50'>
+                            <th className='text-center p-2 rounded-tl-lg'>#</th>
+                            <th className='text-left p-2 w-full'>Details</th>
+                            <th className='text-left p-2 rounded-tr-lg'>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody> 
+                        {renderMain()}
+                    </tbody>
+                </table>
             </div>
         </div>
-    )
+    );
 }
 
-export default Tasks
-
-/*
- <div>
-            <h1 className='text-5xl text-white font-bold text-center'>Tasks</h1>
-            <div className='grid grid-cols-3 gap-2'>
-                {renderMain()}
-            </div>
-        </div>
-*/
+export default Tasks;
