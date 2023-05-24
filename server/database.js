63de1d04e62c9194039591f91e11673390d3dddb
Ana Juárez <lisa13suarez@gmail.com>
2023-05-24 15:55:07 -0600

    actualizando datos

--- server/database.js
+++ server/database.js
@@ -4,6 +4,6 @@ export const pool = createPool({
     host: process.env.DB_HOST || "localhost", //127.0.0.1
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER ||"root",
-    password: process.env.DB_PASSWORD ||"ana1234",
-    database: process.env.DB_DATABASE ||"tasksdb"
+    password: process.env.DB_PASSWORD ||"ana1234", //ana1234
+    database: process.env.DB_DATABASE ||"tasks"
 });
